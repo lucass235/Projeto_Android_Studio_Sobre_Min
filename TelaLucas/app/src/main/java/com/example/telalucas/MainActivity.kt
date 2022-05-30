79e700a6cec148b0b072a4e0e21cab6aa0f1cd17
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-05-29 21:17:25 -0300

    criação do botão sobre min

--- TelaLucas/app/src/main/java/com/example/telalucas/MainActivity.kt
+++ TelaLucas/app/src/main/java/com/example/telalucas/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.telalucas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.telalucas.databinding.ActivityMainBinding
+import com.example.telalucas.databinding.ModalAboutMeBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnShowAboutMe.setOnClickListener{showModal()}
+
+    }
+
+    private fun showModal() {
+        val dialog = BottomSheetDialog(this)
+
+        val modalAboutMe: ModalAboutMeBinding =
+            ModalAboutMeBinding.inflate(layoutInflater, null, false)
+
+        dialog.setContentView(modalAboutMe.root)
+        dialog.show()
     }
 }
