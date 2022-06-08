2ea63ed9885753e29f4c5655f8fbbbb78fa00fbd
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-06-08 10:50:36 -0300

    Atualizações do modal

--- TelaLucas/app/src/main/java/com/example/telalucas/MainActivity.kt
+++ TelaLucas/app/src/main/java/com/example/telalucas/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.telalucas.databinding.ActivityMainBinding
 import com.example.telalucas.databinding.ModalAboutMeBinding
+import com.example.telalucas.databinding.ModalCareerBinding
+import com.example.telalucas.databinding.ModalTechnologiesBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +18,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnShowAboutMe.setOnClickListener{showModal()}
+        binding.btnShowAboutMe.setOnClickListener{showModalAbout()}
+
+        binding.btnShowCoreer.setOnClickListener{showModalCoreer()}
+
+        binding.btnShowTechnologie.setOnClickListener{showModalTechnologie()}
 
     }
 
-    private fun showModal() {
+    private fun showModalAbout() {
         val dialog = BottomSheetDialog(this)
 
         val modalAboutMe: ModalAboutMeBinding =
@@ -28,4 +35,25 @@ class MainActivity : AppCompatActivity() {
         dialog.setContentView(modalAboutMe.root)
         dialog.show()
     }
+
+    private fun showModalCoreer() {
+        val dialog = BottomSheetDialog(this)
+
+        val modalCorrer: ModalCareerBinding =
+            ModalCareerBinding.inflate(layoutInflater, null, false)
+
+        dialog.setContentView(modalCorrer.root)
+        dialog.show()
+    }
+
+    private fun showModalTechnologie() {
+        val dialog = BottomSheetDialog(this)
+
+        val modalTechnologie: ModalTechnologiesBinding =
+            ModalTechnologiesBinding.inflate(layoutInflater, null, false)
+
+        dialog.setContentView(modalTechnologie.root)
+        dialog.show()
+    }
+
 }
